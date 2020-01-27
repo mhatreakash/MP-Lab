2a8be01541560d97007b83e57b693466e462e274
manishjha <manishjha551@gmail.com>
2020-01-27 13:18:41 +0530

    register done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public actionSheetController: ActionSheetController,public alertController: AlertController) {}
 
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Options',
+      buttons: [{
+        text: 'Login',
+        icon: 'heart',
+        handler: () => {
+          console.log('Login clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm',
+      subHeader: 'Welcome',
+      message: 'This is an alert message.',
+      buttons: ['OK', 'Cancel']
+    });
+
+    await alert.present();
+  }
 }
