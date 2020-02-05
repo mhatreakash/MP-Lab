754cc06d487ee5cf251506bd9e5379a423d6a913
manishjha <akash.mhatre@spit.ac.in>
2020-02-05 11:05:30 +0530

    datafetch

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-  data: {name: string, phone: number, email: string, dob: number}
-  constructor() { }
-
+  data: {name: string, phone: number, email: string, dob: string}
+  constructor(private router: Router) { }
+  
   ngOnInit() {
+    this.data = {
+      name: 'Akash',
+      phone: 12345678,
+      email: 'akash.mhatre@spit.ac.in',
+      dob: '28-07-1997'
+    }
   }
 
 }
