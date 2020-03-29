d424f4a2e65c900225e1199c9e74a0c506b05ece
mhatreakash <akash.mhatre@spit.ac.in>
2020-03-29 15:08:40 +0530

    done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,60 +1,133 @@
+import { ToastController, AlertController } from '@ionic/angular';
 import { Component } from '@angular/core';
-import { ActionSheetController, AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+
+export interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-  constructor(public actionSheetController: ActionSheetController,public alertController: AlertController) {}
+  link = 'https://reqres.in/api/users?page=0&per_page=12';
+  users: User[] = [];
+  defaultImage = '../../assets/person.png';
 
-  async presentActionSheet() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Albums',
-      buttons: [{
-        text: 'Delete',
-        role: 'destructive',
-        icon: 'trash',
-        handler: () => {
-          console.log('Delete clicked');
-        }
-      }, {
-        text: 'Share',
-        icon: 'share',
-        handler: () => {
-          console.log('Share clicked');
-        }
-      }, {
-        text: 'Play',
-        icon: 'arrow-dropright-circle',
-        handler: () => {
-          console.log('Play clicked');
-        }
-      }, {
-        text: 'Favorite',
-        icon: 'heart',
-        handler: () => {
-          console.log('Favorite clicked');
-        }
-      }, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        }
-      }]
+  constructor(private http: HttpClient, private toastController: ToastController, private alertController: AlertController) {
+    http.get(this.link).subscribe(data => {
+      this.users = data['data'];
     });
-    await actionSheet.present();
   }
 
-  async presentAlert() {
+  deleteUser(id) {
+    const name = this.users[id].first_name + ' ' + this.users[id].last_name + ' is deleted.';
+    this.users.splice(id, 1);
+    this.presentToast(name, 'danger');
+  }
+
+  async addUser() {
     const alert = await this.alertController.create({
-      header: 'Thanks',
-      subHeader: 'You are now following Akash',
-      message: '',
-      buttons: ['OK']
+      header: 'Add User',
+      inputs: [
+        {
+          name: 'first_name',
+          type: 'text',
+          placeholder: 'First Name'
+        },
+        {
+          name: 'last_name',
+          type: 'text',
+          placeholder: 'Last Name'
+        }, {
+          name: 'email',
+          type: 'email',
+          placeholder: 'Email'
+        }
+
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Add',
+          handler: (data) => {
+            this.users.unshift(data);
+            this.http.post(this.link, data).subscribe(res => {
+              this.presentToast('User added.', 'success')
+            }, error => {
+              this.users.splice(0, 1);
+              this.presentToast('User not added.', 'warning')
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  async presentToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      color,
+      message,
+      duration: 900
+    });
+    toast.present();
+  }
+
+  async editUser(i) {
+    const alert = await this.alertController.create({
+      header: 'Add User',
+      inputs: [
+        {
+          name: 'first_name',
+          type: 'text',
+          value: this.users[i].first_name,
+          placeholder: 'First Name'
+        },
+        {
+          name: 'last_name',
+          type: 'text',
+          value: this.users[i].last_name,
+          placeholder: 'Last Name'
+        }, {
+          name: 'email',
+          type: 'email',
+          value: this.users[i].email,
+          placeholder: 'Email'
+        }
+
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Update',
+          handler: (data) => {
+            this.users[i].first_name = data.first_name;
+            this.users[i].last_name = data.last_name;
+            this.users[i].email = data.email;
+            console.log(this.users[i]);
+            this.presentToast('User updated.', 'success')
+          }
+        }
+      ]
     });
 
     await alert.present();
